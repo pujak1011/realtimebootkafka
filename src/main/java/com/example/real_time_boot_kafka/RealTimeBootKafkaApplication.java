68991efbccf0c050ff99c53fdc7a445feaef098f
pujak1011 <puja.it.10@gmail.com>
2024-08-01 20:18:07 +0530

    initial setup

---
+++ src/main/java/com/example/real_time_boot_kafka/RealTimeBootKafkaApplication.java
@@ -0,0 +1,13 @@
+package com.example.real_time_boot_kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealTimeBootKafkaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RealTimeBootKafkaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/real_time_boot_kafka/RealTimeBootKafkaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.real_time_boot_kafka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealTimeBootKafkaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
