34f4bb791c0376bf43c12f510c54245909c57407
pujak1011 <puja.it.10@gmail.com>
2024-08-02 11:21:08 +0530

    changes for kafka producer and consumer

--- kafka-producer-wiki/src/main/java/com/example/SpringBootProducerApplication.java
+++ kafka-producer-wiki/src/main/java/com/example/SpringBootProducerApplication.java
@@ -1,11 +1,25 @@
 package com.example;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootProducerApplication {
+public class SpringBootProducerApplication implements CommandLineRunner {
     public static void main(String[] args) {
         SpringApplication.run(SpringBootProducerApplication.class);
+    }
+
+    @Autowired
+    private WikiChangesProducer wikiChangesProducer;
+    /**
+     * @param args
+     * @throws Exception
+     */
+
+    @Override
+    public void run(String... args) throws Exception {
+        wikiChangesProducer.sendMessage();
     }
 }
